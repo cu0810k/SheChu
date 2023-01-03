87d189016016f5adb9aa4cfd34a741919c3c2103
Your NameTzu <cu0810k@gmail.com>
2023-01-03 11:57:23 +0800

    add ec other column

--- main.js
+++ main.js
@@ -39,10 +39,16 @@ document.querySelector('.btn-ec-event').addEventListener('click', e => {
   let event_name = document.querySelector("#ec_event_name").value;
   let currency = document.querySelector("#currency").value;
   let value = document.querySelector("#value").value;
+  let coupon = document.querySelector("#coupon").value;
+  let payment_type = document.querySelector("#payment_type").value;
+  let transaction_id = document.querySelector("#transaction_id").value;
+  let tax = document.querySelector("#tax").value;
+  let shipping = document.querySelector("#shipping").value;
+
   let item_id = document.querySelector("#item_id").value;
   let item_name = document.querySelector("#item_name").value;
   let affiliation = document.querySelector("#affiliation").value;
-  let coupon = document.querySelector("#coupon").value;
+  let item_coupon = document.querySelector("#item_coupon").value;
   let item_currency = document.querySelector("#item_currency").value;
   let discount = document.querySelector("#discount").value;
   let index = document.querySelector("#index").value;
@@ -60,19 +66,27 @@ document.querySelector('.btn-ec-event').addEventListener('click', e => {
   let quantity = document.querySelector("#quantity").value;
   dataLayer.push(
     {
-      "event": event,
-      "event_name": event_name,
-      "currency": currency,
-      "value": Number(value),
+      "event": event ? event : undefined,
+      "event_name": event_name ? event_name : undefined,
+      "currency": currency ? currency : undefined,
+      "value": Number(value) ? Number(value) : undefined,
+      
+      "coupon": coupon ? coupon : undefined,
+      "payment_type": payment_type ? payment_type : undefined,
+      "transaction_id": transaction_id ? transaction_id : undefined,
+      "tax": Number(tax) ? Number(tax) : undefined,
+      "shipping": Number(shipping) ? Number(shipping) : undefined,
+
+
       "items": [
         {
           item_id: item_id ? item_id : undefined,
           item_name: item_name ? item_name : undefined,
           affiliation: affiliation ? affiliation : undefined,
-          coupon: coupon ? coupon : undefined,
-          currency: item_currency ? currency : undefined,
-          discount: Number(discount),
-          index: Number(index),
+          coupon: item_coupon ? item_coupon : undefined,
+          currency: item_currency ? item_currency : undefined,
+          discount: Number(discount) ? Number(discount) : undefined,
+          index: Number(index) ? Number(index) : undefined,
           item_brand: item_brand ? item_brand : undefined,
           item_category: item_category ? item_category : undefined,
           item_category2: item_category2 ? item_category2 : undefined,
@@ -83,10 +97,12 @@ document.querySelector('.btn-ec-event').addEventListener('click', e => {
           item_list_name: item_list_name ? item_list_name : undefined,
           item_variant: item_variant ? item_variant : undefined,
           location_id: location_id ? location_id : undefined,
-          price: Number(price),
-          quantity: Number(quantity)
+          price: Number(price) ? Number(price) : undefined,
+          quantity: Number(quantity) ? Number(quantity) : undefined
         }
       ]
     }
   )
 })
+
+
